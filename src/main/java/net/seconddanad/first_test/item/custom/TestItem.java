7f146f1bde04daf0bf2b1df7db75b2098a8a6847
Almale02 <gaspardaniel080716@gmail.com>
2023-06-20 19:48:49 +0200

    2023/06/20.2

--- src/main/java/net/seconddanad/first_test/item/custom/TestItem.java
+++ src/main/java/net/seconddanad/first_test/item/custom/TestItem.java
@@ -1,21 +1,14 @@
 package net.seconddanad.first_test.item.custom;
 
 import net.minecraft.block.BlockState;
-import net.minecraft.block.entity.*;
-import net.minecraft.entity.Entity;
+import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.block.entity.SignBlockEntity;
 import net.minecraft.entity.LivingEntity;
-import net.minecraft.entity.passive.PigEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.ItemUsageContext;
-import net.minecraft.nbt.NbtCompound;
-import net.minecraft.nbt.NbtElement;
-import net.minecraft.nbt.NbtList;
-import net.minecraft.nbt.NbtType;
-import net.minecraft.server.world.ServerWorld;
 import net.minecraft.state.property.Properties;
-import net.minecraft.state.property.Property;
 import net.minecraft.text.Text;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
@@ -41,16 +34,18 @@ public class TestItem extends Item {
             BlockEntity blockEntity = eventWorld.getBlockEntity(eventBlockPos);
 
             if (blockEntity instanceof SignBlockEntity) {
-                //Direction dir = eventBlockState.get(Properties.FACING);
-                sendMessageToPlayer(eventPlayer, "asdf");
+                Direction dir = eventBlockState.get(Properties.HORIZONTAL_FACING);
+                sendMessageToPlayer(eventPlayer, String.valueOf(dir));
 
-                //sendMessageToPlayer(eventPlayer, dir.toString());
                 SignBlockEntity sign = ((SignBlockEntity) blockEntity);
 
                 sign.setText(sign.getText(true), false);
                 sign.getBackText().withColor(sign.getFrontText().getColor());
                 sign.getBackText().withGlowing(sign.getFrontText().isGlowing());
             } else {
+                Direction dir = eventBlockState.get(Properties.HORIZONTAL_FACING);
+                sendMessageToPlayer(eventPlayer, String.valueOf(dir));
+
                 sendMessageToPlayer(eventPlayer, "not sign");
             }
             
